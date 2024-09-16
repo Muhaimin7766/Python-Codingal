953feb3360f5cacd57ee2faab215fc8b5071820f
Muhaimin7766 <muhaiminalam2@gmail.com>
2024-09-16 12:54:14 +0600

    Add files via upload

---
+++ Mirrored Triangle.py
@@ -0,0 +1,8 @@
+print("Mirrored Half Pyramid Pattern of Stars (*)")
+n = int(input("Enter the number of rows: "))
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
