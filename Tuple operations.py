da1e1afd9d9cf2fc48f3e59de26215ba4e1e8c10
Muhaimin7766 <muhaiminalam2@gmail.com>
2024-10-03 20:21:56 +0600

    Add files via upload

---
+++ Tuple operations.py
@@ -0,0 +1,18 @@
+tuplex=("tuple", False, 6.7, 1)
+print(tuplex)
+
+tuplex=(8, 9, 2, 1, 4, 4)
+print(tuplex)
+print(tuplex)
+
+tuple1=(21, 13, 11, 12, 4, 6)
+print(tuple1.count(13))
+
+tuplex=(2, 5, 9, 7, 4, 9, 5, 1, 8, 6)
+_slice=tuplex[3:5]
+print(_slice)
+_slice=tuplex[:6]
+print(_slice)
+
+slice2= tuplex[1:-5]
+print(slice2)
