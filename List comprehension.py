23280712599d888de9fe7e417917b6a62ee42f50
Muhaimin7766 <muhaiminalam2@gmail.com>
2024-10-13 20:46:23 +0600

    Add files via upload

---
+++ List comprehension.py
@@ -0,0 +1,16 @@
+empty_list=[]
+print()
+
+numbers=[1, 2, 3, 4, 5]
+print(numbers)
+
+triples=[1, 2, 3]*3
+print(triples)
+
+aList=[101, 202, 303, 404, 505]
+aList=aList[::-1]
+print(aList, "\n")
+
+print(aList[1])
+print(aList[2])
+print(aList[4])
